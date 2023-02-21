3a01ddf8b04b9394a5d7a802964ce47290b45af9
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-02-21 11:56:25 -0300

    update
    update

--- ediaristas/administracao/views/usuario_views.py
+++ ediaristas/administracao/views/usuario_views.py
@@ -1,5 +1,5 @@
 from ..forms.usuario_forms import UsuarioForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 
 def cadastrar_usuario(request):
@@ -7,13 +7,16 @@ def cadastrar_usuario(request):
     form_usuario = UsuarioForm(request.POST)
     if form_usuario.is_valid():
       form_usuario.save()
+      return redirect('listar_usuarios')
   else:
     form_usuario = UsuarioForm()
   return render(request, 'usuarios/form_usuario.html', {'form_usuario': form_usuario})
 
 def listar_usuarios(request):
   User = get_user_model()
-  usuario = User.objects.all()
-  
+  usuarios = User.objects.all()
+  return render(request, 'usuarios/lista_usuarios.html', {'usuarios': usuarios})
+
 # Analisa-se dentro da lib a função base UserCreationForm
 # Estamos utilizando get_user_model para identificar o tipo de autenticação da api para fazer a validção, assim evitando a quebra da API administrativa em caso de alteração do tipo de authenticação
+# User = get_user_model(), aqui estsamos pegando o modo de authenticação do sistema
