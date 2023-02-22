d9d067c6f490af9d3f2364d595e829e46aab7d76
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-02-21 23:06:46 -0300

    update
    update

--- ediaristas/administracao/views/usuario_views.py
+++ ediaristas/administracao/views/usuario_views.py
@@ -17,14 +17,14 @@ def listar_usuarios(request):
   usuarios = User.objects.filter(is_superuser=True)
   return render(request, 'usuarios/lista_usuarios.html', {'usuarios': usuarios})
 
-def editar_usuarios(request, id):
+def editar_usuario(request, id):
   User = get_user_model()
   usuario = User.objects.get(id=id)
   form_usuario = EditarUsuarioForm(request.POST or None, instance=usuario)
   if form_usuario.is_valid():
     form_usuario.save()
     return redirect('listar_usuario') 
-  return render(request, 'usuarios/form_usuarios.html', {'form_usuario': form_usuario})  
+  return render(request, 'usuarios/editar_usuario.html', {'form_usuario': form_usuario})  
   
   
   
