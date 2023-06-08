cdc9c5ae924fe2bbd374944d6d4e468cabed200a
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-06-08 18:52:10 -0300

    update
    update

--- ediaristas/api/views/disponibilidade_atendimento_cidade.py
+++ ediaristas/api/views/disponibilidade_atendimento_cidade.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..services import cidades_atendimento_service
+from rest_framework import status as status_http
 
 #aqui estamos retornando um True ou False, onde vai verificar se o CEP tem disponibilidade de atendimento ou não
 class DisponibilidadeAtendimentoCidade(APIView):
@@ -8,4 +9,9 @@ class DisponibilidadeAtendimentoCidade(APIView):
     #aqui vamos ter localhost:8000/api/diaristas/disponibilidade?cep={cep}
     cep = self.request.query_params.get('cep', None)
     disponibilidade = cidades_atendimento_service.verificar_disponibilidade_cidade(cep)
-    return Response({"disponibilidade": disponibilidade})
+    return Response({
+      "code": "success",
+      "disponibilidade": disponibilidade,
+      "message": "Disponibilidade verificada com sucesso",
+      "status":status_http.HTTP_200_OK ,
+    })
