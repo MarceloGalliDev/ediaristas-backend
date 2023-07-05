cb4f1e7a82b707060be10db6df34dddec0b4c24d
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-07-05 19:27:02 -0300

    update
    update

--- ediaristas/api/services/diaria_service.py
+++ ediaristas/api/services/diaria_service.py
@@ -1,4 +1,5 @@
 from ..models import Diaria
+from .usuario_service import listar_usuario_id
 
 # passamos um post e aqui recebemos o objeto de acordo com o ID
 def listar_diaria_id(diaria_id):
@@ -9,4 +10,16 @@ def atualizar_status_diaria(diaria_id, status):
   diaria.status = status #aqui atualizamos o status, atualizando com o status que recebemos como paramêtro
   diaria.save() #aqui salvamos
 
+def listar_diarias_usuario(usuario_id):
+  #usuario logado na aplicação
+  usuario = listar_usuario_id(usuario_id)
+  #vamos verificar o tipo de usuario
+  if usuario.tipo_usuario == 1:
+    #vamos retornar da nossa lista de diárias todas as diárias em que atributo cliente é = usuario.id
+    return Diaria.objects.filter(cliente=usuario.id).all()
+  return Diaria.objects.filter(diariasta=usuario.id).all()
+  
+  
+  
+  
 # Aqui fazemos os ORM de service, para metodologias de négocios
--- ediaristas/api/views/diaria_view.py
+++ ediaristas/api/views/diaria_view.py
@@ -2,6 +2,7 @@
 
 from rest_framework.views import APIView
 from ..serializers import diaria_serializer
+from ..services.diaria_service import listar_diarias_usuario
 from rest_framework.response import Response
 from rest_framework import status as status_http
 from rest_framework import permissions
@@ -22,3 +23,8 @@ class Diaria(APIView):
       return Response(serializer_diaria.data, status=status_http.HTTP_201_CREATED)
     return Response(serializer_diaria.errors, status=status_http.HTTP_400_BAD_REQUEST)
   
+  def get(self, request, format=None):
+    diarias = listar_diarias_usuario(request.user.id)
+    #many=True é para indicar que vamos indicar mais de um objeto
+    serializer_diaria = diaria_serializer.DiariaSerializer(diarias, many=True, context={'request': request})
+    return Response(serializer_diaria.data, status=status_http.HTTP_200_OK)
